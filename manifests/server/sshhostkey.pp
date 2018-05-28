c8c60b28ba116e7ecb04bb7189de31ab2573d1fc
William Kimball <30981667+wwkimball@users.noreply.github.com>
2018-05-28 12:04:10 -0500

    Trying to be compatible with ghoneycutt-ssh...

--- manifests/server/sshhostkey.pp
+++ manifests/server/sshhostkey.pp
@@ -2,7 +2,8 @@ class sshfs::server::sshhostkey {
   # Export this server's SSH host key so all clients can import it.  Without
   # this, all SSH connections to this sshfs server will hang, waiting for human
   # review and manual acceptance of this key.
-  @@sshkey { $::facts['networking']['fqdn']:
+  @@sshkey { "sshfs-server-${::facts['networking']['fqdn']}":
+    name => $::facts['networking']['fqdn'],
     type => 'ecdsa-sha2-nistp256',
     key  => $::facts['ssh']['ecdsa']['key'],
     tag  => 'sshfs-server-key',
