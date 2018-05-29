a2bbc31f18d11d1de8467b16b5d5581db6224d2d
William Kimball <30981667+wwkimball@users.noreply.github.com>
2018-05-28 22:49:13 -0500

    Added rsa key along with ecdsa key

--- manifests/server/sshhostkey.pp
+++ manifests/server/sshhostkey.pp
@@ -2,10 +2,17 @@ class sshfs::server::sshhostkey {
   # Export this server's SSH host key so all clients can import it.  Without
   # this, all SSH connections to this sshfs server will hang, waiting for human
   # review and manual acceptance of this key.
-  @@sshkey { "sshfs-server-${::facts['networking']['fqdn']}":
+  @@sshkey { "sshfs-server-ecdsa-${::facts['networking']['fqdn']}":
     name => $::facts['networking']['fqdn'],
     type => 'ecdsa-sha2-nistp256',
     key  => $::facts['ssh']['ecdsa']['key'],
+    tag  => 'sshfs-server-key',
+  }
+
+  @@sshkey { "sshfs-server-rsa-${::facts['networking']['fqdn']}":
+    name => $::facts['networking']['fqdn'],
+    type => 'ssh-rsa',
+    key  => $::facts['ssh']['rsa']['key'],
     tag  => 'sshfs-server-key',
   }
 }
